9d1e6f1c5b2db0060d379a0cf2df3802343388e3
kaptarartom-stack <324378294+kaptarartom-stack@users.noreply.github.com>
2026-09-18 22:14:00 +0300

    second-task-coord

--- lab1/LabFirst.java
+++ lab1/LabFirst.java
@@ -16,6 +16,41 @@ float m = in.nextFloat();
 
 float result = (float) (((n + 1) * (m - 4) + n * m - Math.pow(n, 4) + Math.pow(m, 3)) / Math.pow((m + 2), 2));
 System.out.println("Результат=" + result);
+// робота з масивом, що має координати і радіусом на початку координат(0;0)
+System.out.print("Введіть розмір масиву: ");
+int size = in.nextInt();
+if (size % 2 == 0 && size <= 200) {
+
+    float[] arr = new float[size];
+
+    System.out.println("Введіть елементи масиву:");
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = in.nextFloat();
+    }
+
+    System.out.println("Введіть радіус кола:");
+    float r = in.nextFloat();
+
+    int count = 0;
+
+    for (int i = 0; i < size; i += 2) {
+        float x = arr[i];
+        float y = arr[i + 1];
+
+        if (x * x + y * y <= r * r) {
+            count++;
+        }
+    }
+
+    System.out.println("Кількість точок, що належать площині кола: " + count);
+
+} else {
+    System.out.println(
+        "Або заданий розмір масиву більше 200, " +
+        "або розмір масиву непарний"
+    );
+}
 in.close();
 }
 }
